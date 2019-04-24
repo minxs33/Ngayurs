a6fad5d5f56449990963ad4c28b26aa252e7193a
NaufalAlwan <theminxs12@gmail.com>
2019-04-24 09:00:38 +0700

    tambah artikel

---
+++ newSayur/database/migrations/2019_04_24_014109_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->unsignedinteger('user_id');
+
+            $table->increments('id_artikel');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('thumbnail',255);
+            $table->longtext('judul');
+            $table->string('penulis',45);
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
